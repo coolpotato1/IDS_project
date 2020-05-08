8cdb96b4b03d634d0c07ca0dd853d09f52b7cc54
Henning <s153612@student.dtu.dk>
2020-05-08 15:20:09 +0200

    Implemented a sliding window approach to data collection, which seems to be working. Needs more testing though

--- process_pcaps.py
+++ process_pcaps.py
@@ -1,98 +1,236 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 24 10:42:39 2020
+Created on Fri Apr  3 10:44:02 2020
 
 @author: Henning
 """
-#import pcapkit as p
-from scapy.all import *
-from scapy.utils import *
-from scapy.utils6 import *  # noqa: F401
-from scapy.route6 import *
+import arff
+import pyshark
 import re
-#import dpkt
-#import pyshark as p  
-data = rdpcap("pcaps/radiolog-1585743076838.pcap")
-#pcap_file = open("pcaps/radiolog-1585038898097.pcap", "rb")
-#data = dpkt.pcap.Reader(pcap_file)
+import numpy as np
+from _collections import defaultdict
+from dataset_manipulation import export_attacks
+# import asyncio
+# import nest_asyncio
+own_simulation = True
+ATTACK_PROTOCOL = "udp" if own_simulation else "udp"
+#ATTACKER_IDS = "209:9:9:9" if own_simulation else ["19", "12", "17", "0c", "11", "0b", "15", "16", "10", "0d"]
+#ATTACKER_IDS = "209:9:9:9" if own_simulation else ["19", "18", "13"]
+ATTACKER_IDS = "209:9:9:9" if own_simulation else ["0c", "0b", "09", "12", "18", "15", "13", "19", "17", "0f", "10", "11"]
+BORDER_ID = "201:1:1:1" if own_simulation else "01:1:101"
+ATTACK_DELAY = 480 - 1 if own_simulation else 0 # Minus a second, because to find the start of the attack, we use the first packets timestamp, which is likely not 0
+ATTACK_TYPE = "sinkhole"
+data = pyshark.FileCapture("pcaps/radiolog-1586451632600.pcap")
+
 
 class flow:
-        src_bytes = 0
-        dest_bytes = 0
-        protocol = ""
-        
-def get_node_specific_udp(packet_list):
-    node_specific_udp = defaultdict(list)
-    for packet in packet_list:
-        if(packet.haslayer(UDP)):
-            #The next line only ensures unique node ID's up till 16^2 nodes
-            node_id = str(hex(packet[Dot15d4].src_addr))[-2] + str(hex(packet[Dot15d4].src_addr))[-1]
-            node_specific_udp[node_id].append(str(packet[Raw].load) + "\n")
-    return node_specific_udp
+    src_bytes = 0
+    dst_bytes = 0
+    protocol = ""
+    flow_class = "normal"
+    dio_count = 0
+    dao_count = 0
+    dis_count = 0
 
-#Takes a packet data dictionary 
-def get_counters(packet_dict):
-    counters = defaultdict(list)
-    for key in packet_dict:
-        counters[key] = [int(re.search("\(\((\\d*)\)\)", data_str).group(1)) for data_str in packet_dict[key] if re.search("\(\((\\d*)\)\)", data_str) != None]
-        
-    return counters
+    def get_flow_as_list(self):
+        return [self.src_bytes, self.dst_bytes, self.protocol, self.dio_count, self.dao_count, self.dis_count, self.flow_class]
 
-#Packets between the 2 same hosts should be the same, even if they switch up 
-#which host is src and which is dest. For this reason we sort the hosts.
+    # This should probably be refactored at some point
+    @staticmethod
+    def get_flow_attributes():
+        return [("src_bytes", "REAL"), ("dst_bytes", "REAL"), ("protocol_type", ["tcp", "udp", "icmpv6"]),
+                ("dio_count", "REAL"), ("dao_count", "REAL"), ("dis_count", "REAL"), ("class", ["normal", "anomaly"])]
+
+
+# Husk at ændre 0 og 43
+transport_protocols = {
+    # "0": "Hop-by-hop Option",
+    "6": "tcp",
+    "17": "udp",
+    # "43": "Routing header for ipv6",
+    "58": "icmpv6",
+    "121": "smp"
+}
+
+
+# Packets between the 2 same hosts should be in the same flow, even if they switch up
+# which host is src and which is dest. For this reason we sort the hosts (as we use their combined addresses as a key for their flow).
 def sort_addresses(src_ip, dest_ip):
-    sorted_list = sorted([src_ip, dest_ip], reverse = True)
-    #Check if the ordering has been changed, needed later to determine src and dest bytes
+    sorted_list = sorted([src_ip, dest_ip], reverse=True)
+    # Check if the ordering has been changed, needed later to determine src and dest bytes
     is_reversed = sorted_list[0] == dest_ip
     return is_reversed, sorted_list
-    
+
+#this is needed because the ips in old cooja are stupid
+def get_svelte_ips(hex_ids):
+    return ["::212:74" + id + ":" + id.lstrip("0") + ":" + id.lstrip("0") + id for id in hex_ids]
+
+
+def get_protocol(packet):
+    for key in transport_protocols:
+        if transport_protocols[key] in packet:
+            return transport_protocols[key]
+
+def add_rpl_info(flow, packet):
+    if "icmpv6" not in packet:
+        return
+
+    if packet.icmpv6.code == "0":
+        flow.dis_count += 1
+    elif packet.icmpv6.code == "1":
+        flow.dio_count += 1
+    elif packet.icmpv6.code == "2":
+        flow.dao_count +=1
+    elif packet.icmpv6.cod == "3":
+        pass
+    else:
+        print("unexpected icmpv6 code")
+
+
+def is_packet_at_final_destination(packet):
+    #The wpan address property name depends on the adress mode, so we need to check for that.
+    if packet.wpan.dst_addr_mode[-1] == "3":
+        #This is kinda a hacky solution, but it is necessary because the ip of border router is ::1, so cannot just match on the last 2 characters as with the other ip's
+        return packet.wpan.dst64[-1] == packet.ipv6.dst[-1] and (packet.wpan.dst64[-2] == packet.ipv6.dst[-2] or
+               (packet.wpan.dst64[-2] == "0" and packet.ipv6.dst[-2] == ":"))
+
+    return False
+
+def does_packet_fulfill_requirements(packet):
+    return "ipv6" in packet and "ff02" not in packet.ipv6.dst \
+           and (packet.ipv6.nxt != "43" or packet.ipv6.routing_segleft == "0")
+
+
+#This uses a sliding window approach, so there is overlap between flows which is why flow_step and flow_length are not equal
+#Also, best to use a flow length that is divisible for flow step (Am currently not sure if doing otherwise could cause issues
+def add_packet_to_flows(flow_dict, packet, flow_length, flow_step, start_time):
+    time_since_start = float(packet.sniff_timestamp) - start_time
+    current_flow_step = time_since_start - time_since_start % flow_step
+
+    # Oldest flow step goes 1 step too far back, but it evens out as np.arrange() does not include it
+    # -flow_step/2 is necessary, to allow np.arange to hit 0
+    oldest_flow_step_included = np.maximum(-flow_step/2, current_flow_step - flow_length)
+    flows_to_add_packet = np.arange(current_flow_step, oldest_flow_step_included, -flow_step)
+
+    # First one checks if necessary protocols are there and if it is at final destination for one protocol, second
+    # one checks for other protocols if they are at final destination. Should probably be refactered to one method.
+    # The reason we check for final destination, is so that we do not count a packet after every hop
+    if not does_packet_fulfill_requirements(packet) or not is_packet_at_final_destination(packet):
+        return
+
+    protocol = get_protocol(packet)
+    is_reversed, temp_flow_identifier = sort_addresses(packet.ipv6.src, packet.ipv6.dst)
+
+    for flow_step in flows_to_add_packet:
+        flow_identifier = str(flow_step) + ";" + temp_flow_identifier[0] + ";" + temp_flow_identifier[
+            1] + ";" + protocol
+
+        flow_dict[flow_identifier].protocol = protocol
+        flow_dict[flow_identifier].dst_bytes += int(packet.ipv6.plen) if is_reversed else 0
+        flow_dict[flow_identifier].src_bytes += int(packet.ipv6.plen) if not is_reversed else 0
+        add_rpl_info(flow_dict[flow_identifier], packet)
+
+
 def get_flows(raw_data):
     flow_dict = defaultdict(flow)
-    src_addresses = []
-    src_addresses2 = []
+    is_first = True
     for packet in raw_data:
-        if(packet.haslayer(IPv6)):
-            is_reversed, temp_flow_identifier = sort_addresses(str(hex(packet[Dot15d4].src_addr)), str(hex(packet[Dot15d4].dest_addr)))
-            flow_identifier = temp_flow_identifier[0] + temp_flow_identifier[1] + str(ipv6nh[packet[IPv6].nh])
-            flow_dict[flow_identifier].protocol = packet[IPv6].nh
-            flow_dict[flow_identifier].dest_bytes += packet[IPv6].plen if is_reversed else 0
-            flow_dict[flow_identifier].src_bytes += packet[IPv6].plen if not is_reversed else 0
-        if(packet.haslayer(UDP)):
-            src_addresses.append(packet[IPv6].src)
-            #src_addresses2.append(packet[LoWPAN].sourceAddr)
-        
-    return flow_dict, src_addresses, src_addresses2
-        
-    
+        if is_first:
+            start_time = float(packet.sniff_timestamp)
+            is_first = False
+
+        add_packet_to_flows(flow_dict, packet, flow_length=30, flow_step=1, start_time=start_time)
+
+    return flow_dict
+
+
+def is_anomaly(flow_key):
+    if own_simulation:
+        time_stamp = float(re.search("^[^;]+", flow_key).group(0))
+        if time_stamp >= ATTACK_DELAY:
+            if ATTACKER_IDS in flow_key and ATTACK_PROTOCOL in flow_key:
+                return True
+
+        return False
+
+    else:
+        is_blocked = False
+        for ip in get_svelte_ips(ATTACKER_IDS):
+            if ip in flow_key:
+                is_blocked = True
+
+        if is_blocked and ATTACK_PROTOCOL in flow_key:
+            return True
+        else:
+            return False
+
+
+def label_flows(flow_dict):
+    for key in flow_dict:
+        if is_anomaly(key):
+            flow_dict[key].flow_class = "anomaly"
+
+
+#This function returns a dictionary with the actual data carried in the packets, without any headers
+def get_data_dict(raw_data):
+    data_dict = defaultdict(list)
+
+    for packet in raw_data:
+        if "udp" in packet:
+            data = bytearray.fromhex(packet.data.data).decode()
+            data_dict[packet.ipv6.src].append(data)
+
+    return data_dict
+
 def get_packet_loss(packet_dict):
-     received_packets = 0
-     sent_packets = 0
-     
-     for key in packet_dict:
-         pattern = "\(\((\\d*)\)\)"
-         i = -1
-         
-         #Search for the latest packet which contains a counter
-         while(re.search(pattern, packet_dict[key][i]) == None):
-             i-=1
-         
-         match =  re.search(pattern, packet_dict[key][i])
-         
-         if(key == "01"):
-             received_packets = int(match.group(1))
-         else:
-             sent_packets += int(match.group(1))
-             
-     return ((sent_packets-received_packets)/sent_packets) * 100
-    
+    received_packets = 0
+    sent_packets = 0
 
-#data_dict = get_node_specific_udp(data)
+    for key in packet_dict:
+        pattern = "\(\((\\d*)\)\)"
+        i = -1
 
-#print(get_packet_loss(data_dict))
-     
-#counters = get_counters(data_dict)
-#for key in counters:
-#    print(key, ": ", counters[key][-1])
-flows, src_addresses, src_addresses2 = get_flows(data)
+        # Search for the latest packet which contains a counter
+        while re.search(pattern, packet_dict[key][i]) is None:
+            i -= 1
 
+        match = re.search(pattern, packet_dict[key][i])
+
+        if BORDER_ID in key:
+            received_packets = int(match.group(1))
+        else:
+            sent_packets += int(match.group(1))
+
+    return ((sent_packets - received_packets) / sent_packets) * 100
+
+# def get_packet_loss(raw_data):
+#     received_packets = 0
+#     sent_packets = 0
+#     for packet in raw_data:
+#         if "ipv6" in packet:
+#             if packet.ipv6.nxt == "43" and packet.ipv6.routing_segleft != "0":
+#                 print(packet.sniff_timestamp)
+
+
+def export_as_arff(flow_dict, file):
+    attributes = flow.get_flow_attributes()
+    data = [flow_dict[key].get_flow_as_list() for key in flow_dict]
+
+    export_arff = {
+        'relation': 'CoojaData',
+        'description': 'The simulated data from the IoT environment',
+        'data': data,
+        'attributes': attributes
+    }
+
+    arff.dump(export_arff, open("Datasets/" + file + ".arff", "w+"))
+    print("exported datasets")
+
+    export_attacks([ATTACK_TYPE if flow_dict[key].flow_class == "anomaly" else "normal" for key in flow_dict],
+                   "Datasets/" + file + "_attacks")
+
+flows = get_flows(data)
+label_flows(flows)
+export_as_arff(flows, "coojaData1")
+print("debugging point")
 
